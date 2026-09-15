4b248427c74b79f7f81fb9a516efa33c00182dfb
Dagohoy <jewellevincentatienza09@gmail.com>
2026-09-15 22:32:23 +0800

    fix hero model

--- src/Sections/Hero.tsx
+++ src/Sections/Hero.tsx
@@ -65,10 +65,10 @@ export default function Hero() {
                     </div>
                 </header>
                 <figure>
-                    <div className="absolute xl:right-[6%] right-[20%] xl:top-[40%] top-[80%] -translate-y-1/2 xl:w-2xl w-[min(60vw,42rem)] aspect-square">
+                    <div className="absolute xl:right-[4%] right-[20%] xl:top-[40%] top-[80%] -translate-y-1/2 xl:w-2xl w-[min(60vw,42rem)] aspect-square">
                         <ShapedPinBadge
                             imageUrl="/images/pin-photo.png"
-                            size={2.5}
+                            size={2.8}
                             cameraDistance={5}
                         />
                     </div>
